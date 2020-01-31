5c13d232250ee75a155010d9c8164f8418397ee8
Ekaterina Karpenko <karpenkokatri6a@gmail.com>
2020-01-31 17:35:56 +0300

    Locators for login link were transfered to BasePageLocators for using on any page

--- pages/locators.py
+++ pages/locators.py
@@ -1,7 +1,11 @@
 from selenium.webdriver.common.by import By
 
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
 class MainPageLocators():
-    LOGIN_LINK= (By.CSS_SELECTOR, "#login_link")
+    pass
 
 class LoginPageLocators():
     LOGIN_FORM= (By.CSS_SELECTOR, "#login_form")
@@ -13,3 +17,5 @@ class ProductPageLocators():
     PRODUCT_PRICE= (By.CSS_SELECTOR, "p.price_color")
     ADDED_PRODUCT_NAME=(By.CSS_SELECTOR,"div.alert-success:nth-child(1) strong")
     ADDED_PRODUCT_PRICE=(By.CSS_SELECTOR, "div.alert-info strong")
+
+    SUCCESS_MESSAGE=(By.CSS_SELECTOR, "div.alert-success:nth-child(1)")
